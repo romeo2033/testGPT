d5710042e716cf2f32a58b4604a9bb3c7e2e00d1
Роман Перепонов <roman.pereponov15@gmail.com>
2024-12-06 20:27:27 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+"""Файл конфигурации"""
+
+# Токен бота, полученный у BotFather в Telegram
+TOKEN = "<TG_TOKEN>"
+# Токен API OpenAI для доступа к ChatGPT
+openaiTOKEN = "<OPEN_AI_TOKEN>"
+# Путь к файлу с Базой данных пользователей
+db = "gpt.db"
